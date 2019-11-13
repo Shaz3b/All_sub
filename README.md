d8471b8a44521242ac93e12007e5f9c8b7f00e09
root <root@kali.localdomain>
2019-11-13 18:04:39 +0000

    Added certspotter

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SubAll 
+ SubAll is a tool, which i used to run and collect all the subdomins using Sublister, crtsh and cerspotter.
+ It will create one file called domains.txt, which containts all the outputs in one file.
+
+## Usage
+ ./SubAll.sh -d target.com`
--- SubAll.sh
+++ SubAll.sh
@@ -7,6 +7,7 @@ fi
 DOMAIN=$1
 red=`tput setaf 1`
 green=`tput setaf 2`
+
 banner(){
   echo "${red}
    ____        _       _    _ _ 
@@ -19,20 +20,30 @@ banner(){
 
 sublister(){
 
-  echo "${green}[+]Starting Sublister...."
+  echo "[*]Starting Sublister...."
   rm -rf ~/recon/${DOMAIN} && mkdir ~/recon/${DOMAIN}
   cd ~/extra_tools/Sublist3r/
   python sublist3r.py -d ${DOMAIN} -t 10 -v -o ~/recon/${DOMAIN}/domains.txt > /dev/null
 }
 
 crtsh(){
-  echo "${green}[+]starting crtsh and appending all the domain into the domains.txt file."
+  echo "[*]Checking crtsh and appending to domains.txt."
   curl -s https://crt.sh/?q=%.${DOMAIN}  | sed 's/<\/\?[^>]\+>//g' | grep ${DOMAIN} >> ~/recon/${DOMAIN}/domains.txt
 }
 
+certspotter(){
+ echo "[*]Checking certspotter and appending to domains.txt."
+ curl -s https://certspotter.com/api/v0/certs\?domain\=${DOMAIN} | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep ${DOMAIN} >> ~/recon/${DOMAIN}/domains.txt
+ }
+
 banner
+echo "${green}Starting to collect all subdomins using Sublist3r, crtsh and certspotter."
 sublister
 crtsh
+certspotter
+
+
+echo "Output saved in ~/recon/${DOMAIN}/"
 
 if [ sublister != 0 ];
 then
@@ -40,4 +51,3 @@ then
 fi
 
 
-echo "Output saved in ~/recon/${DOMAIN}/"
